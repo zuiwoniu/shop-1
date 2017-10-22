968c026a16238232d0e8b806814bf2cfa31ca5a5
Happylhuan <1170088794@qq.com>
2017-10-22 20:53:30 +0800

    修改userAction

--- Shop/src/com/huan/business/action/userAction.java
+++ Shop/src/com/huan/business/action/userAction.java
@@ -1,12 +1,34 @@
 package com.huan.business.action;
 
+import java.util.List;
+
 import com.huan.business.po.SUsers;
+import com.huan.business.service.IUserService;
+import com.huan.tool.PageBean;
+import com.huan.tool.PageModel;
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.ModelDriven;
 
 public class userAction extends ActionSupport implements ModelDriven<SUsers>{
 
 	private SUsers user = new SUsers();
+	private IUserService userService;
+	private List<SUsers> users;
+	private PageModel pageModel;
+	private PageBean page = new PageBean(1, 5);
+	@SuppressWarnings("unchecked")
+	public String listUser(){
+		
+		pageModel = userService.listUser();
+		users = (List<SUsers>) pageModel.getObject();
+		if (users != null) {
+			return "list";
+		} else {
+			this.addActionError("²éÑ¯¿Í»§Ê§°Ü");
+			return INPUT;
+		}
+		
+	}
 	@Override
 	public SUsers getModel() {
 		// TODO Auto-generated method stub
